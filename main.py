ec201d8520822de119ec1d287acadfdcf007a01a
mohammed sharaki <67878024+MohammedSharaki@users.noreply.github.com>
2023-01-20 19:22:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+array_1=np.array([1,2,3,4,5])
+array_2=np.array([11,21,31,41,51])
+array_3=np.array([12,22,32,42,52])
+array_4=np.array([array_1,array_2,array_3])
+print(array_4.shape())
